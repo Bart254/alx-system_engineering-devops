13ec6289072aedfce09ea0908def6037b6de03ea
Bart254 <omondibartlett@gmail.com>
2023-12-27 12:45:02 +0300

    correct manifest in file 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,5 @@
 # Puppet manifest to install Flask version 2.1.0 using pip3
-package {'python3-pip':
-name   => 'python3-pip'
-ensure => installed,
-}
-package {'flask':
-name     => 'Flask',
+package {'python3-flask':
 ensure   => '2.1.0',
 provider => 'pip3',
-require  => Package['python3-pip'],
 }
