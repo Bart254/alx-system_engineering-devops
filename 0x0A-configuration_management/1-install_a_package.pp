1f71ffb3d4469243288e2d8ff329676d7f6ad893
Bart254 <omondibartlett@gmail.com>
2023-12-27 12:34:40 +0300

    add package python-pip as a requirement'

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
 # Puppet manifest to install Flask version 2.1.0 using pip3
+package {'python3-pip':
+ensure => installed,
+name   => 'python3-pip',
+}
 package {'flask':
 ensure   => '2.1.0',
+name     => 'Flask',
 provider => 'pip3',
-name     => 'flask',
+require  => Package['python3-pip'],
 }
