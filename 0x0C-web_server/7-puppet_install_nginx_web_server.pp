a0b5ab67e1ac207cbd4e15c5d9ac1ae24ea15867
Bart254 <omondibartlett@gmail.com>
2024-01-09 13:31:54 +0300

    remove all root privileges

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -2,19 +2,10 @@
 # Ensure the package is installed
 package { 'nginx':
   ensure  => installed,
-  require => Exec['sudo update and install']
-}
-
-exec { 'sudo update and install':
-  command => 'sudo apt-get -y update && sudo apt-get -y install nginx',
-  path    => ['/usr/bin', '/usr/local/bin'],
-  unless  => 'which nginx'
 }
 
 file { '/etc/nginx/sites-available/default':
   ensure  => present,
-  owner   => 'root',
-  group   => 'root',
   mode    => '0644',
   require => Package['nginx'],
   content => '# Configures server blocks for websites
@@ -39,27 +30,25 @@ server {
  		root /usr/share/nginx/html;
  		internal;
  	}
-}'
+}',
+  notify  => Service['nginx']
 }
 
 file { '/usr/share/nginx/html/custom_404.html':
   ensure  => present,
-  owner   => 'root',
-  group   => 'root',
   mode    => '0644',
   content => "Ceci n'est pas une page\n",
   require => File['/etc/nginx/sites-available/default']
 }
 
-exec { 'ufw allow Nginx HTTP':
-  command => "sudo ufw allow 'Nginx HTTP'",
-  path    => ['/usr/bin', '/usr/local/bin'],
-  require => Package['nginx'],
-  before  => Service['nginx']
-}
+#exec { 'ufw allow Nginx HTTP':
+#  command => 'sudo ufw allow Nginx HTTP',
+#  path    => ['/usr/bin', '/usr/local/bin'],
+#  require => Package['nginx'],
+#  before  => Service['nginx']
+#}
 
 service { 'nginx':
-  ensure    => running,
-  enable    => true,
-  subscribe => File['/etc/nginx/sites-available/default']
+  ensure => running,
+  enable => true,
 }
