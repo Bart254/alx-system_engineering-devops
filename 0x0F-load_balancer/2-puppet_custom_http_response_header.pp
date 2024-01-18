eb6cd4eca6430afcac8fd3a3b8b39f1dfdbe5477
Bart254 <omondibartlett@gmail.com>
2024-01-19 00:03:47 +0300

    only add header

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,51 +1,12 @@
-# Automatically installs and configures nginx using puppet
-# Ensure the package is installed
-package { 'nginx':
-  ensure  => installed,
-}
-
-file { '/etc/nginx/sites-available/default':
-  ensure  => present,
-  mode    => '0644',
-  require => Package['nginx'],
-  content => "# Configures server blocks for websites
-server {
-	listen 80 default_server;
-
-	add_header X-Served-By $::hostname;
-
-	location /redirect_me {
-		return 301 https://www.youtube.com/watch?v=QH2-TGULwu4;
-	}
-
-	location / {
-		root /var/www/html;
-		index index.html;
-	}
-
-	error_page 404 /custom_404.html;
-
-	location = /custom_404.html {
-		root /usr/share/nginx/html;
-		internal;
- 	}
-}",
-  notify  => Service['nginx']
-}
-
-file { '/var/www/html/index.html':
-  ensure  => present,
-  content => "Hello World!\n"
-}
-
-file { '/usr/share/nginx/html/custom_404.html':
-  ensure  => present,
-  mode    => '0644',
-  content => "Ceci n'est pas une page\n",
-  require => File['/etc/nginx/sites-available/default']
+# Adds header to a web_server
+file_line { '/etc/nginx/sites-available/default':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  line   => "\tadd_header X-Served-By ${::hostname};",
 }
 
 service { 'nginx':
-  ensure => running,
-  enable => true,
+  enable    => true,
+  ensure    => running,
+  subscribe => File_line['/etc/nginx/sites-available/default']
 }
