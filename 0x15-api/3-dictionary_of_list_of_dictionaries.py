61a231f547c3fc9a7dc0aa46071c4fb079e6481b
Bart254 <omondibartlett@gmail.com>
2024-01-30 02:03:39 +0300

    add all users todo list

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Gathers data from Employee API
+"""
+if __name__ == "__main__":
+    import json
+    import requests
+
+    users = []
+    user_resp = requests.get('https://jsonplaceholder.typicode.com/users')
+    for dic in user_resp.json():
+        user_dict = {'id': dic["id"], 'username': dic["username"]}
+        users.append(user_dict)
+    json_file = 'todo_all_employees.json'
+    my_dict = {}
+    with open(json_file, 'a') as f:
+        for user in users:
+            id = user['id']
+            my_dict[id] = []
+            todo_values = {'userId': id}
+            todo_req = 'https://jsonplaceholder.typicode.com/todos/'
+            todo_resp = requests.get(todo_req, params=todo_values)
+            for dic in todo_resp.json():
+                new_dic = {
+                            "task": dic["title"],
+                            "completed": dic["completed"],
+                            "username": user['username']
+                            }
+                my_dict[id].append(new_dic)
+        json.dump(my_dict, f)
