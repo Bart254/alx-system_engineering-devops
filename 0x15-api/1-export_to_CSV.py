894c2274b75a103f5bc8028e23f3ad5041ececb0
Bart254 <omondibartlett@gmail.com>
2024-01-30 01:05:36 +0300

    add csv file

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Gathers data from Employee API
+"""
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    id = sys.argv[1]
+    todo_values = {'userId': id}
+    todo_resp = requests.get('https://jsonplaceholder.typicode.com/todos/',
+                             params=todo_values)
+    user_values = {'id': id}
+    user_resp = requests.get('https://jsonplaceholder.typicode.com/users/',
+                             params=user_values)
+    name = user_resp.json()[0]['name']
+    csv_file = id+'.csv'
+    with open(csv_file, 'a') as f:
+        for dic in todo_resp.json():
+            string = '"{}","{}","{}","{}"\n'.format(id, name, dic["completed"],
+                                                    dic["title"])
+            f.write(string)
