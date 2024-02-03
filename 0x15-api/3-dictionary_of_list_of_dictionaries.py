fc10a424a0362ef9cce69463bbc63f7644a76e0c
Bart254 <omondibartlett@gmail.com>
2024-02-03 17:29:54 +0300

    update 3-dictionary_of_list_of_dictionaries.py

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -5,25 +5,24 @@ if __name__ == "__main__":
     import json
     import requests
 
-    users = []
-    user_resp = requests.get('https://jsonplaceholder.typicode.com/users')
-    for dic in user_resp.json():
-        user_dict = {'id': dic["id"], 'username': dic["username"]}
-        users.append(user_dict)
-    json_file = 'todo_all_employees.json'
-    my_dict = {}
-    with open(json_file, 'a') as f:
-        for user in users:
-            id = user['id']
-            my_dict[id] = []
-            todo_values = {'userId': id}
-            todo_req = 'https://jsonplaceholder.typicode.com/todos/'
-            todo_resp = requests.get(todo_req, params=todo_values)
-            for dic in todo_resp.json():
-                new_dic = {
-                            "username": user['username'],
-                            "task": dic["title"],
-                            "completed": dic["completed"],
-                            }
-                my_dict[id].append(new_dic)
-        json.dump(my_dict, f)
+    user_data = {}
+    todo_resp = requests.get("https://jsonplaceholder.typicode.com/todos/")
+    user_resp = requests.get("https://jsonplaceholder.typicode.com/users/")
+    json_file = "todo_all_employees.json"
+
+    for user in user_resp.json():
+        user_id = user["id"]
+        username = user["username"]
+        key = str(user_id)
+        value = []
+        for todo in todo_resp.json():
+            if todo["userId"] == user_id:
+                my_dict = {"username": username,
+                           "task": todo["title"],
+                           "completed": todo["completed"]
+                           }
+                value.append(my_dict)
+        user_data[key] = value
+
+    with open(json_file, 'w') as file:
+        json.dump(user_data, file)
