f238f79e289d853487c3c4db7efb213fb7704daf
Bart254 <omondibartlett@gmail.com>
2024-03-06 17:09:22 +0300

    add puppet config file

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,8 +1,14 @@
 # Adds header to a web_server
+package { 'nginx':
+  ensure => installed,
+}
+
 file_line { '/etc/nginx/sites-available/default':
-  ensure => present,
-  path   => '/etc/nginx/sites-available/default',
-  line   => "\tadd_header X-Served-By ${::hostname};",
+  ensure   => present,
+  path     => '/etc/nginx/sites-available/default',
+  match    => '^server {',
+  line     => "server {\n\tadd_header X-Served-By ${::hostname};",
+  multiple => false
 }
 
 service { 'nginx':
