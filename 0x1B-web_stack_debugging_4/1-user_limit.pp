b8cf1a8d6b375e0205e22740c354f115c87d4efc
Bart254 <omondibartlett@gmail.com>
2024-03-21 16:38:27 +0300

    fix the syntax

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -6,12 +6,12 @@ user { 'holberton':
 exec { 'increase-hard-file-limit-for-holberton-user':
   command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
   path    => ['/usr/local/bin', '/bin'],
-  require   => User['holberton']
+  require => User['holberton']
 }
 
 #Increase soft file limit for user holberton
 exec { 'increase-soft-file-limit-for-holberton-user':
   command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
   path    => ['/usr/local/bin', '/bin'],
-  require   => User['holberton']
+  require => User['holberton']
 }
