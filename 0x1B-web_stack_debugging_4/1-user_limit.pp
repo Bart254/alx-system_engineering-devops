42f27e06e7d4e0001716a571d1d8202743b3ae11
Bart254 <omondibartlett@gmail.com>
2024-03-21 16:37:08 +0300

    add file 1-user_limit.pp

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,17 @@
+# Creates a user and ensures smooth access
+
+user { 'holberton':
+    ensure => present
+}
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => ['/usr/local/bin', '/bin'],
+  require   => User['holberton']
+}
+
+#Increase soft file limit for user holberton
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => ['/usr/local/bin', '/bin'],
+  require   => User['holberton']
+}
