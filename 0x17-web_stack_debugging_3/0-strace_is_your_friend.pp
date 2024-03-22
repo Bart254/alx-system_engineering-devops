508d316edb2de943610483a9a0d05b57712ec306
Bart254 <113597942+Bart254@users.noreply.github.com>
2024-03-22 06:26:39 +0300

    Delete 0x17-web_stack_debugging_3 directory
    Deferring

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,6 +0,0 @@
-# Fixes an error with wp-settings
-# An execution command
-exec { 'fix_phpp_error':
-  path    => ['/bin/', '/usr/bin/', '/usr/local/bin'],
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php'
-}
