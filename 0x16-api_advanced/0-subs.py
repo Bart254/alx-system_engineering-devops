af9efe687319b1d402cf9a83383996484d208503
Bart254 <omondibartlett@gmail.com>
2024-04-09 20:26:29 +0300

    add authorization

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -16,8 +16,8 @@ def number_of_subscribers(subreddit):
     -------
     Number of subscribers to a subreddit
     """
-    url = f'https://oauth.reddit.com/r/{subreddit}/about.json'
-    headers = {"User-Agent": "Castro-Python"}
+    url = f'https://oauth.reddit.com/r/{subreddit}/about'
+    headers = {"User-Agent": "Castro-Python", "Authorization": "bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IlNIQTI1NjpzS3dsMnlsV0VtMjVmcXhwTU40cWY4MXE2OWFFdWFyMnpLMUdhVGxjdWNZIiwidHlwIjoiSldUIn0.eyJzdWIiOiJ1c2VyIiwiZXhwIjoxNzEyNzYxMzg4LjY5OTY1NywiaWF0IjoxNzEyNjc0OTg4LjY5OTY1NywianRpIjoiU1lnUVluWVd6UXllMmlGRl94WEM2N3ZKRzJTbC13IiwiY2lkIjoiekRZeVJpQmlEeUpLRmJhVjZOMUdpZyIsImxpZCI6InQyX3h5YXExMzhydSIsImFpZCI6InQyX3h5YXExMzhydSIsImxjYSI6MTcxMjY3MTc0NzQ2MSwic2NwIjoiZUp5S1Z0SlNpZ1VFQUFEX193TnpBU2MiLCJmbG8iOjl9.jke9Ls5juh5zEwYct3b-mjnsljrbue0lQcQP7QxTSHBMLXDuiAipJzNWSToz99yK4-2yclVrjqcoeyYRinm3BhAahZ6V0WphCxTGjst7hpdkZ8ZMkzW8Liq7UHRVDGzJbDSSWKxy-58NZ-YIDXkV8cQkVek0nqV25xDtzvmYGiIAku9bcBal0edCB2ooLEQyvmpJRHPZb4Zdj5iOrx0KtYWn1Hqyu0OAxeD0M_c2FOyAMqs8gXVS2oOLNALdM8FJ5-8yTSJNSgkeRiFgvXwQfM3RIpdg6ib4yIfPDqtaTwsNfn_Z2-yij_bYeOTMMvFFxzjG2sARhQ9d9JkZACmvfg"}
 
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
