ae20efba0d4b921b6e4b887710f1f15994c4320c
Bart254 <omondibartlett@gmail.com>
2024-04-09 19:47:00 +0300

    add file 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,26 +1,32 @@
 #!/usr/bin/python3
-"""Returns the number of subs to a subreddit
+"""Number of subs to a subreddit
 """
 import requests
 
 
 def number_of_subscribers(subreddit):
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {'User-Agent': 'my-app/0.0.1'}
+    """Returns number of subscribers to subreddit
+
+    Parameters
+    ----------
+    subreddit: str
+        The subreddit whose subsribers are to be found
+
+    Returns
+    -------
+    Number of subscribers to a subreddit
+    """
+    url = f'https://oauth.reddit.com/r/{subreddit}/about.json'
+    headers = {"User-Agent": "Castro-Python"}
 
     response = requests.get(url, headers=headers)
-
-    # Check if the response status code indicates success
     if response.status_code == 200:
         data = response.json()
-
-        # Check if the response contains the expected data
         if 'data' in data and 'subscribers' in data['data']:
             return data['data']['subscribers']
         else:
             return 0
     elif response.status_code == 302:
-        # Redirect to search results, indicating an invalid subreddit
         return 0
     else:
         return 0
